192409d89008a2367a7e31028f1843d3ff6519a6
Evenandjust <xzzwmjl@gmail.com>
2017-10-18 14:22:36 -0700

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,71 @@
+package com.franks.restaurantmenu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/*
+ * @ Created By: Frank
+ * @ Date: 10/16/2017
+ * @ Function: Digital Menu for restaurant (Owner use)
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+    public void addDishButtonClicked(View view){
+        Intent addDishIntent = new Intent(MainActivity.this, AddDish.class);
+        startActivity(addDishIntent);
+    }
+
+    public void viewOrdersButtonClicked(View view){
+        Intent viewOrders = new Intent(MainActivity.this, ViewOrders.class);
+        startActivity(viewOrders);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ ViewOrders.java
@@ -0,0 +1,65 @@
+package com.franks.restaurantmenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ViewOrders extends AppCompatActivity {
+
+    private RecyclerView myOrdersList;
+    private DatabaseReference myDBRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_orders);
+
+        myOrdersList = (RecyclerView) findViewById(R.id.ordersList);
+        myOrdersList.setHasFixedSize(true);
+        myOrdersList.setLayoutManager(new LinearLayoutManager(this));
+        myDBRef = FirebaseDatabase.getInstance().getReference().child("Orders");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseRecyclerAdapter<Order, OrderViewHolder> fRAdapter = new FirebaseRecyclerAdapter<Order, OrderViewHolder>(
+                Order.class, R.layout.singleorder, OrderViewHolder.class, myDBRef
+        ) {
+            @Override
+            protected void populateViewHolder(OrderViewHolder viewHolder, Order model, int position) {
+                viewHolder.setUsername(model.getUsername());
+                viewHolder.setDishname(model.getDishname());
+            }
+        };
+
+        myOrdersList.setAdapter(fRAdapter);
+    }
+
+    public static class OrderViewHolder extends RecyclerView.ViewHolder{
+        View orderView;
+
+        public OrderViewHolder(View itemView) {
+            super(itemView);
+            orderView = itemView;
+        }
+
+        public void setUsername(String username) {
+            TextView usernameText = (TextView) itemView.findViewById(R.id.orderUsername);
+            usernameText.setText(username);
+        }
+
+        public void setDishname(String dishname){
+            TextView dishnameText = (TextView) itemView.findViewById(R.id.orderDishname);
+            dishnameText.setText(dishname);
+        }
+    }
+}
